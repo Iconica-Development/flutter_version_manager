4a0d828c0f327eb66d440c8e1bd94366aa604f28
Niels <niels@iconica.nl>
2024-11-12 10:36:47 +0100

    feat: setup repository

---
+++ packages/flutter_version_manager/lib/flutter_version_manager.dart
@@ -0,0 +1,7 @@
+library flutter_version_manager;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ packages/version_repository_interface/lib/version_repository_interface.dart
@@ -0,0 +1,7 @@
+library version_repository_interface;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
