dabba7415f8aee71952885401fbd40ac0770f61c
Freek van de Ven <freek@freekvandeven.nl>
2025-03-05 09:49:09 +0100

    feat: rework the dialogbuilders for optional updates

--- packages/flutter_version_manager/lib/src/ui/widgets/optional_update_dialog.dart
+++ packages/flutter_version_manager/lib/src/ui/widgets/optional_update_dialog.dart
@@ -1,62 +1,86 @@
 import "package:flutter/material.dart";
 import "package:flutter_version_manager/src/utils/scope.dart";
-import "package:version_repository_interface/version_repository_interface.dart";
 
-///
-Future<bool> defaultOptionalUpdate(
-  BuildContext context, {
-  required VersionCompatibiliy compatibility,
-  required bool backendLeading,
-}) async {
-  if (backendLeading) {
-    var config = VersionManagerScope.of(context).config;
-    var translations = config.translations;
-    ScaffoldMessenger.of(context).showSnackBar(
-      config.optionalUpdateSnackBarBuilderBackendleading?.call(context) ??
-          SnackBar(
-            content: Text(
-              translations.optionalUpdateBackendLeadingTitle,
-            ),
-          ),
-    );
+/// Backend Leading Optional Update Dialog
+class DefaultOptionalUpdateDialogBackendLeading {
+  ///
+  const DefaultOptionalUpdateDialogBackendLeading._();
+
+  ///
+  static Future<bool> showOptionalUpdateDialog(BuildContext context) async {
+    var scope = VersionManagerScope.of(context);
+    var config = scope.config;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        config.optionalUpdateSnackBarBuilderBackendleading.call(context),
+      );
+    });
 
     return false;
-  } else {
-    return await showDialog<bool>(
-          context: context,
-          builder: (ctx) {
-            var config = VersionManagerScope.of(context).config;
-            var translations = config.translations;
-            return config.optionalUpdateDialogBuilderFrontendleading
-                    ?.call(ctx) ??
-                AlertDialog(
-                  title: Text(
-                    translations.optionalUpdateFrontendLeadingTitle,
-                  ),
-                  content: Text(
-                    translations.optionalUpdateFrontendLeadingBody,
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(true);
-                      },
-                      child: Text(
-                        translations.optionalUpdateBackendLeadingYes,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(false);
-                      },
-                      child: Text(
-                        translations.optionalUpdateBackendLeadingNo,
-                      ),
-                    ),
-                  ],
-                );
+  }
+
+  ///
+  static SnackBar builder(BuildContext context) {
+    var scope = VersionManagerScope.of(context);
+    var translations = scope.config.translations;
+
+    return SnackBar(
+      content: Text(
+        translations.optionalUpdateBackendLeadingTitle,
+      ),
+    );
+  }
+}
+
+/// Frontend Leading Optional Update Dialog
+class DefaultOptionalUpdateDialogFrontendLeading {
+  ///
+  const DefaultOptionalUpdateDialogFrontendLeading._();
+
+  ///
+  static Future<bool> showOptionalUpdateDialog(BuildContext context) async {
+    var scope = VersionManagerScope.of(context);
+    var config = scope.config;
+
+    return await showDialog(
+      context: context,
+      builder: (ctx) =>
+          config.optionalUpdateDialogBuilderFrontendleading.call(context),
+    );
+  }
+
+  ///
+  static Widget builder(BuildContext context) {
+    var scope = VersionManagerScope.of(context);
+    var translations = scope.config.translations;
+
+    return AlertDialog(
+      title: Text(
+        translations.optionalUpdateFrontendLeadingTitle,
+      ),
+      content: Text(
+        translations.optionalUpdateFrontendLeadingBody,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(true);
           },
-        ) ??
-        false;
+          child: Text(
+            translations.optionalUpdateBackendLeadingYes,
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+          child: Text(
+            translations.optionalUpdateBackendLeadingNo,
+          ),
+        ),
+      ],
+    );
   }
 }
