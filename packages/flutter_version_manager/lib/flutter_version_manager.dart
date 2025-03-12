4213974b04eb1259e0191a311b03b0a73b5ee44b
nick-iconica <nick@iconica.nl>
2025-03-12 11:32:34 +0100

    feat(flutter_version_manager): add open_store functionality

--- packages/flutter_version_manager/lib/flutter_version_manager.dart
+++ packages/flutter_version_manager/lib/flutter_version_manager.dart
@@ -5,4 +5,5 @@ export "src/config/builders.dart";
 export "src/config/translations.dart";
 export "src/config/version_manager_config.dart";
 export "src/flutter_version_manager.dart";
+export "src/utils/open_store.dart";
 export "src/utils/scope.dart";
